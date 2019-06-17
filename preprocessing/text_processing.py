934ce24114ab440577de35582ab5457bf4e1cf39
msmilevski <m.smilevski57@gmail.com>
2019-06-17 14:06:14 +0100

    Add func for transforming sentences to idx

--- preprocessing/text_processing.py
+++ preprocessing/text_processing.py
@@ -6,7 +6,7 @@ from transliterate import translit
 from tqdm import tqdm
 import json
 import operator
-
+import h5py
 
 def preprocess_line(line, reg, mystem=None):
     line = line.lower()
@@ -76,56 +76,77 @@ def create_word_frequency_document(path_to_file, path_to_json_file='../dataset/w
 
 
 def get_n_most_frequent_words(word_frequency_file='../dataset/word_frequencies.json', vocabulary_size=10000):
+    data = json.load(open(word_frequency_file))
+    return data[0:vocabulary_size]
 
-        data = json.load(open(word_frequency_file))
-        return data[0:vocabulary_size]
 
 def generate_vocabulary(vocabulary_file='../dataset/vist2017_vocabulary.json',
-                            word_frequency_file='../dataset/word_frequencies.json', vocabulary_size=10000):
+                        word_frequency_file='../dataset/word_frequencies.json', vocabulary_size=10000):
+    data = get_n_most_frequent_words(word_frequency_file, vocabulary_size)
 
-        data = get_n_most_frequent_words(word_frequency_file, vocabulary_size)
+    idx_to_words = []
+    idx_to_words.append("<NULL>")
+    idx_to_words.append("<START>")
+    idx_to_words.append("<END>")
+    idx_to_words.append("<UNK>")
 
-        idx_to_words = []
-        idx_to_words.append("<NULL>")
-        idx_to_words.append("<START>")
-        idx_to_words.append("<END>")
-        idx_to_words.append("<UNK>")
+    for element in data:
+        idx_to_words.append(element[0])
 
-        for element in data:
-            idx_to_words.append(element[0])
+    words_to_idx = {}
+    for i in range(len(idx_to_words)):
+        words_to_idx[idx_to_words[i]] = i
 
-        words_to_idx = {}
-        for i in range(len(idx_to_words)):
-            words_to_idx[idx_to_words[i]] = i
+    vocabulary = {}
+    vocabulary["idx_to_words"] = idx_to_words
+    vocabulary["words_to_idx"] = words_to_idx
 
-        vocabulary = {}
-        vocabulary["idx_to_words"] = idx_to_words
-        vocabulary["words_to_idx"] = words_to_idx
+    with open(vocabulary_file, 'w') as fp:
+        json.dump(vocabulary, fp)
 
-        with open(vocabulary_file, 'w') as fp:
-            json.dump(vocabulary, fp)
 
-# def sentences_to_index_helper(self, sentence, word_to_idx, max_length):
-#         words = sentence.split()
-#         result_sentence = []
-#
-#         for word in words:
-#             if len(result_sentence) == max_length:
-#                 break
-#             else:
-#                 if (word_to_idx.has_key(word)):
-#                     result_sentence.append(word_to_idx[word])
-#                 else:
-#                     result_sentence.append(word_to_idx["<UNK>"])
-#
-#         result_sentence.insert(0, word_to_idx["<START>"])
-#         result_sentence.append(word_to_idx["<END>"])
-#
-#         while len(result_sentence) < max_length + 2:
-#             result_sentence.append(word_to_idx["<NULL>"])
-#
-#         return result_sentence
-#
+def sent_to_idx_helper(sentence, word_to_idx, max_length):
+    words = sentence.split()
+    result_sentence = []
+    for word in words:
+        if len(result_sentence) == max_length:
+            break
+        else:
+            if (word_to_idx.has_key(word)):
+                result_sentence.append(word_to_idx[word])
+            else:
+                result_sentence.append(word_to_idx["<UNK>"])
+
+    result_sentence.insert(0, word_to_idx["<START>"])
+    result_sentence.append(word_to_idx["<END>"])
+
+    while len(result_sentence) < max_length + 2:
+        result_sentence.append(word_to_idx["<NULL>"])
+
+    return result_sentence
+
+
+def sentences_to_indecies(dataset_file_path, processed_text_file_path, vocab_file, max_length):
+    reader = pd.read_csv(dataset_file_path, chunksize=100, encoding='utf-8')
+    data = json.load(open(vocab_file))
+    word_to_idx = data['word_to_idx']
+    item_id = []
+    descriptions = []
+
+    for batch in tqdm(reader):
+        batch_descriptions = batch['descriptions']
+        ids = batch['itemID'].tolist()
+
+        for i, desc in enumerate(batch_descriptions):
+            item_id.append(ids[i])
+            descriptions.append(desc, word_to_idx, max_length)
+
+    print("Saving file: " + processed_text_file_path + " ...")
+    data_file = h5py.File(processed_text_file_path, 'w')
+    data_file.create_dataset("itemID", data=item_id)
+    data_file.create_dataset("descriptions", data=descriptions)
+
+
 # def indecies_to_sentence(self, sentence, idx_to_word):
 #
 #         result_sentence = ""
@@ -138,4 +159,5 @@ def generate_vocabulary(vocabulary_file='../dataset/vist2017_vocabulary.json',
 
 # preprocess_corpus(sys.argv[1], sys.argv[2], sys.argv[3])
 # create_word_frequency_document(sys.argv[1], sys.argv[2])
-generate_vocabulary(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+# generate_vocabulary(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+sentences_to_indecies(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
