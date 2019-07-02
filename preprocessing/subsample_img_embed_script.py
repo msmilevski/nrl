1899b0b404f66c99196637e428bd40f276a79e89
msmilevski <m.smilevski57@gmail.com>
2019-07-03 00:10:52 +0100

    Fix bug

--- preprocessing/subsample_img_embed_script.py
+++ preprocessing/subsample_img_embed_script.py
@@ -7,8 +7,9 @@ import sys
 def get_image_embedding(images_dir, image_id):
     folder_id = image_id % 100
     with h5py.File(images_dir + "/image_features_" + str(folder_id) + ".hdf5", 'r') as img_data:
-        position_item = np.argwhere(img_data['image_id'].value == image_id)[0][0]
-        return img_data['image_features'].value[position_item]
+        ids = img_data['image_id'][()]
+        position_item = np.argwhere(ids == image_id)[0][0]
+        return img_data['image_features'][position_item]
 
 
 pair_file_path = sys.argv[1]
