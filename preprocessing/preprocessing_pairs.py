2145febd5e5342aac7ecbc9e4349df1624f7fa3a
msmilevski <m.smilevski57@gmail.com>
2019-07-04 22:09:51 +0100

    Refactor

--- preprocessing/preprocessing_pairs.py
+++ preprocessing/preprocessing_pairs.py
@@ -24,8 +24,8 @@ if num_subsamples != -1:
     additional_prefix = "subsample"
 
 if do_split:
-    print("Split the dataset to training and validation...")
-    # Make train valid split
+    print("Split the dataset to training, validation and test sets.")
+    # Make train valid test split
     train_dataset, val_dataset, test_dataset = preprocessing.split_to_train_val(dataframe)
     train_dataset.to_csv("dataset/avito-duplicate-ads-detection/" + additional_prefix + "ItemPairs_train_processed.csv", encoding='utf-8')
     val_dataset.to_csv("dataset/avito-duplicate-ads-detection/" + additional_prefix + "ItemPairs_val_processed.csv", encoding='utf-8')
