c5eba9172b686f48ae5978feeef74dcc035377bf
msmilevski <m.smilevski57@gmail.com>
2019-07-04 19:47:06 +0100

    fix

--- catch_file.py
+++ catch_file.py
@@ -3,13 +3,25 @@ import os
 from glob import glob
 import sys
 
-root_dir = sys.argv[1]
-paths = [y for x in os.walk(root_dir) for y in glob(os.path.join(x[0], "*.jpg"))]
+paths = ['/home/s1885778/nrl/dataset/Images_/Images_9/95/',
+         '/home/s1885778/nrl/dataset/Images_/Images_8/84/',
+         '/home/s1885778/nrl/dataset/Images_/Images_8/81/',
+         '/home/s1885778/nrl/dataset/Images_/Images_7/74/',
+         '/home/s1885778/nrl/dataset/Images_/Images_0/6/',
+         '/home/s1885778/nrl/dataset/Images_/Images_6/67/',
+         '/home/s1885778/nrl/dataset/Images_/Images_6/61/',
+         '/home/s1885778/nrl/dataset/Images_/Images_4/41/',
+         '/home/s1885778/nrl/dataset/Images_/Images_3/36/',
+         '/home/s1885778/nrl/dataset/Images_/Images_1/13/',
+         '/home/s1885778/nrl/dataset/Images_/Images_1/14/']
 
-for img_path in paths:
-    img_id = int(img_path.split("/")[-1].split(".")[0])
-    image = cv2.imread(img_path)
+for root_dir in paths:
+    paths = [y for x in os.walk(root_dir) for y in glob(os.path.join(x[0], "*.jpg"))]
 
-    if image is None:
-        print(img_path)
+    for img_path in paths:
+        img_id = int(img_path.split("/")[-1].split(".")[0])
+        image = cv2.imread(img_path)
+
+        if image is None:
+            print(img_path)
 
