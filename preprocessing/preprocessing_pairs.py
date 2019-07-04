e0eafb2a37fbf8097b6d5c4ddc893c19479dbf81
Marko Smilevksi <m.smilevski57@gmail.com>
2019-07-04 22:20:18 +0100

    Fix bug

--- preprocessing/preprocessing_pairs.py
+++ preprocessing/preprocessing_pairs.py
@@ -7,7 +7,6 @@ import preprocessing_util as preprocessing
 data_file_path = sys.argv[1]
 indecies_file_path = sys.argv[2]
 num_subsamples = int(sys.argv[3])
-do_split = (sys.argv[4] == 'True')
 # Read file
 print("Read indecies file:" + indecies_file_path)
 ids = h5py.File(indecies_file_path, 'r')['itemID'][:].tolist()
@@ -23,10 +22,9 @@ if num_subsamples != -1:
     dataframe = preprocessing.subsample_data(dataframe, num_subsamples)
     additional_prefix = "subsample"
 
-if do_split:
-    print("Split the dataset to training, validation and test sets.")
-    # Make train valid test split
-    train_dataset, val_dataset, test_dataset = preprocessing.split_to_train_val(dataframe)
-    train_dataset.to_csv("dataset/avito-duplicate-ads-detection/" + additional_prefix + "ItemPairs_train_processed.csv", encoding='utf-8')
-    val_dataset.to_csv("dataset/avito-duplicate-ads-detection/" + additional_prefix + "ItemPairs_val_processed.csv", encoding='utf-8')
-    test_dataset.to_csv("dataset/avito-duplicate-ads-detection/" + additional_prefix + "ItemPairs_test_processed.csv", encoding='utf-8')
+print("Split the dataset to training, validation and test sets.")
+# Make train valid test split
+train_dataset, val_dataset, test_dataset = preprocessing.split_to_train_val_test(dataframe)
+train_dataset.to_csv("dataset/" + additional_prefix + "ItemPairs_train_processed.csv", encoding='utf-8')
+val_dataset.to_csv("dataset/" + additional_prefix + "ItemPairs_val_processed.csv", encoding='utf-8')
+test_dataset.to_csv("dataset/" + additional_prefix + "ItemPairs_test_processed.csv", encoding='utf-8')
