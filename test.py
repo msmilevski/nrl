460eae62a7d06eeca84f90c4349665bc9328c713
msmilevski <m.smilevski57@gmail.com>
2019-07-05 12:35:23 +0100

    Add test script for baseline model

--- test.py
+++ test.py
@@ -1,14 +1,37 @@
 from torch.utils.data import DataLoader
 from data_provider import DatasetProvider
 from torchvision import transforms, utils
+import torch
+import torch.nn as nn
 import sys
+from models import BaselineModel
+from tqdm import tqdm
 
 composed = transforms.ToTensor()
-dataset = DatasetProvider(pair_file_path='dataset/subsampleItemPairs_train_processed.csv',
-                          data_file_path='dataset/fasttext_data_baseline.hdf5',
-                          images_dir='dataset/resnet152/')
+dataset = DatasetProvider(pair_file_path='dataset/ItemPairs_train_processed.csv',
+                          data_file_path='dataset/fasttext_data.hdf5',
+                          images_dir='')
 print(len(dataset))
 dataloader = DataLoader(dataset, batch_size=2, shuffle=True, num_workers=0)
-for i_batch, sample_batched in enumerate(dataloader):
-    print(sample_batched)
-    break
+
+model = BaselineModel(input_dim=2)
+criterion = nn.CrossEntropyLoss()
+model.reset_parameters()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in tqdm(range(20)):
+    model.train()
+    optimizer.zero_grad()
+    for i_batch, sample_batched in enumerate(dataloader):
+        desc_1_batch = sample_batched['desc1']
+        desc_2_batch = sample_batched['desc2']
+        img_1_batch = sample_batched['image_1']
+        img_2_batch = sample_batched['image_2']
+        y_batch = sample_batched['target']
+        x_data = [desc_1_batch, img_1_batch, desc_2_batch, img_2_batch]
+        y_pred = model(x_data)
+        loss = criterion(y_pred, y_batch)
+
+        loss.backward()
+        optimizer.step()
+
