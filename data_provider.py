c7ade1d297160d870e0e6cb0de34aa66eec61ddb
msmilevski <m.smilevski57@gmail.com>
2019-07-09 00:08:33 +0100

    Correcting mistakes in ids

--- data_provider.py
+++ data_provider.py
@@ -32,9 +32,9 @@ class DatasetProvider(Dataset):
         folder_id = image_id % 100
         with h5py.File(self.images_dir + "/image_features_" + str(folder_id) + ".hdf5", 'r') as img_data:
         #with h5py.File('dataset/subsampled_train_img_embed.hdf5', 'r') as img_data:
-            ids = img_data['img_id'][()]
+            ids = img_data['image_id'][()]
             position_item = np.argwhere(ids == image_id)[0][0]
-            return img_data['img_embed'][position_item]
+            return img_data['image_features'][position_item]
 
     def __len__(self):
         return self.pairs.shape[0]
