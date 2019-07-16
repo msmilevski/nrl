f9f2792caa929831e99fc0b46e46347947c7a989
msmilevski <m.smilevski57@gmail.com>
2019-07-16 13:59:30 +0100

    Add model for computing contrastive loss

--- models.py
+++ models.py
@@ -7,64 +7,64 @@ from math import *
 from preprocessing.preprocessing_util import baseline_preprocessing
 
 
-def jaccard_similarity(x, y):
-    x = baseline_preprocessing(x)
-    y = baseline_preprocessing(y)
-    intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-    union_cardinality = len(set.union(*[set(x), set(y)]))
-    return intersection_cardinality / float(union_cardinality)
-
-
-def euclidean_distance(x, y):
-    return sqrt(sum(pow(a - b, 2) for a, b in zip(x, y)))
-
-
-class LogisticRegression(nn.Module):
-    def __init__(self, input_dim):
-        super(LogisticRegression, self).__init__()
-        self.linear = nn.Linear(input_dim, 2)
-
-    def forward(self, x):
-        y_pred = F.sigmoid(self.linear(x))
-        return y_pred
-
-    def reset_parameters(self):
-        self.linear.reset_parameters()
-
-
-class BaselineModel(nn.Module):
-    def __init__(self, input_dim):
-        super(BaselineModel, self).__init__()
-        self.build_model(input_dim=input_dim)
-        self.elements = [0, 1, 2]
-
-    def build_model(self, input_dim):
-        self.model = LogisticRegression(input_dim)
-
-    def feature_engineering(self, x):
-        # return {'desc1': item_1_desc, 'image_1': img_1, 'desc2': item_2_desc, 'image_2': img_2, 'target': y}
-        desc_1_batch = x[0]
-        desc_2_batch = x[2]
-        img_1_batch = x[1]
-        img_2_batch = x[3]
-
-        batch_x = []
-
-        for i in range(len(desc_1_batch)):
-            feature_1 = jaccard_similarity(desc_1_batch[i].numpy(), desc_2_batch[i].numpy())
-            feature_2 = euclidean_distance(img_1_batch[i].numpy(), img_2_batch[i].numpy())
-            batch_x.append([feature_1, feature_2])
-
-        batch_x = torch.from_numpy(np.array(batch_x))
-
-        return batch_x
-
-    def forward(self, x):
-        x = self.feature_engineering(x)
-        return self.model(x.float())
-
-    def reset_parameters(self):
-        self.model.reset_parameters()
+# def jaccard_similarity(x, y):
+#     x = baseline_preprocessing(x)
+#     y = baseline_preprocessing(y)
+#     intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
+#     union_cardinality = len(set.union(*[set(x), set(y)]))
+#     return intersection_cardinality / float(union_cardinality)
+#
+#
+# def euclidean_distance(x, y):
+#     return sqrt(sum(pow(a - b, 2) for a, b in zip(x, y)))
+#
+#
+# class LogisticRegression(nn.Module):
+#     def __init__(self, input_dim):
+#         super(LogisticRegression, self).__init__()
+#         self.linear = nn.Linear(input_dim, 2)
+#
+#     def forward(self, x):
+#         y_pred = F.sigmoid(self.linear(x))
+#         return y_pred
+#
+#     def reset_parameters(self):
+#         self.linear.reset_parameters()
+#
+#
+# class BaselineModel(nn.Module):
+#     def __init__(self, input_dim):
+#         super(BaselineModel, self).__init__()
+#         self.build_model(input_dim=input_dim)
+#         self.elements = [0, 1, 2]
+#
+#     def build_model(self, input_dim):
+#         self.model = LogisticRegression(input_dim)
+#
+#     def feature_engineering(self, x):
+#         # return {'desc1': item_1_desc, 'image_1': img_1, 'desc2': item_2_desc, 'image_2': img_2, 'target': y}
+#         desc_1_batch = x[0]
+#         desc_2_batch = x[2]
+#         img_1_batch = x[1]
+#         img_2_batch = x[3]
+#
+#         batch_x = []
+#
+#         for i in range(len(desc_1_batch)):
+#             feature_1 = jaccard_similarity(desc_1_batch[i].numpy(), desc_2_batch[i].numpy())
+#             feature_2 = euclidean_distance(img_1_batch[i].numpy(), img_2_batch[i].numpy())
+#             batch_x.append([feature_1, feature_2])
+#
+#         batch_x = torch.from_numpy(np.array(batch_x))
+#
+#         return batch_x
+#
+#     def forward(self, x):
+#         x = self.feature_engineering(x)
+#         return self.model(x.float())
+#
+#     def reset_parameters(self):
+#         self.model.reset_parameters()
 
 
 class VQAStandard(nn.Module):
@@ -179,6 +179,21 @@ class SiameseNetwork(nn.Module):
         self.item_2_model.reset_parameters()
         for item in self.layer_dict.children():
             item.reset_parameters()
+
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output_1, output_2, label):
+        # Find the pairwise distance or euclidian distance of two output feature vectors
+        euclidian_distance = F.pairwise_distance(output_1, output_2)
+        # perform constrastive loss calculation with the distsance
+        loss_constrastive = torch.mean((1 - label) * torch.pow(euclidian_distance, 2) + (label) * torch.pow(
+            torch.clamp(self.margin - euclidian_distance, min=0.0), 2))
+
+        return loss_constrastive
 
 # seed = np.random.RandomState(seed=124)
 # embedding_matrix = np.load('dataset/fasttext_embed_10000.npy')
