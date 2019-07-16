eaadbadfbcd157a821e585e1dee3467350889254
msmilevski <m.smilevski57@gmail.com>
2019-07-16 16:05:39 +0100

    Add script for visualisation of precision recall curve

---
+++ plot_precision_recall_curve.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import precision_recall_curve
+import matplotlib.pyplot as plt
+from inspect import signature
+from sklearn.metrics import average_precision_score, confusion_matrix
+
+
+def plot_stats(y_test, y_score, average_precision):
+    tn, fp, fn, tp = confusion_matrix(y_test, y_score).ravel()
+    print((tn, fp, fn, tp))
+    precision, recall, _ = precision_recall_curve(y_test, y_score)
+    step_kwargs = ({'step': 'post'}
+                   if 'step' in signature(plt.fill_between).parameters
+                   else {})
+    plt.clf()
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
+    plt.show()
