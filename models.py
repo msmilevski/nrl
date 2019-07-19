cb58304ae09c99aaa84c9223aa6ff5e96a1943df
msmilevski <m.smilevski57@gmail.com>
2019-07-19 15:05:21 +0100

    Refactoring and fix output of siamese model to be one number

--- models.py
+++ models.py
@@ -1,80 +1,18 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 from torch.nn import functional as F
-from math import *
-from preprocessing.preprocessing_util import baseline_preprocessing
-
-
-# def jaccard_similarity(x, y):
-#     x = baseline_preprocessing(x)
-#     y = baseline_preprocessing(y)
-#     intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-#     union_cardinality = len(set.union(*[set(x), set(y)]))
-#     return intersection_cardinality / float(union_cardinality)
-#
-#
-# def euclidean_distance(x, y):
-#     return sqrt(sum(pow(a - b, 2) for a, b in zip(x, y)))
-#
-#
-# class LogisticRegression(nn.Module):
-#     def __init__(self, input_dim):
-#         super(LogisticRegression, self).__init__()
-#         self.linear = nn.Linear(input_dim, 2)
-#
-#     def forward(self, x):
-#         y_pred = F.sigmoid(self.linear(x))
-#         return y_pred
-#
-#     def reset_parameters(self):
-#         self.linear.reset_parameters()
-#
-#
-# class BaselineModel(nn.Module):
-#     def __init__(self, input_dim):
-#         super(BaselineModel, self).__init__()
-#         self.build_model(input_dim=input_dim)
-#         self.elements = [0, 1, 2]
-#
-#     def build_model(self, input_dim):
-#         self.model = LogisticRegression(input_dim)
-#
-#     def feature_engineering(self, x):
-#         # return {'desc1': item_1_desc, 'image_1': img_1, 'desc2': item_2_desc, 'image_2': img_2, 'target': y}
-#         desc_1_batch = x[0]
-#         desc_2_batch = x[2]
-#         img_1_batch = x[1]
-#         img_2_batch = x[3]
-#
-#         batch_x = []
-#
-#         for i in range(len(desc_1_batch)):
-#             feature_1 = jaccard_similarity(desc_1_batch[i].numpy(), desc_2_batch[i].numpy())
-#             feature_2 = euclidean_distance(img_1_batch[i].numpy(), img_2_batch[i].numpy())
-#             batch_x.append([feature_1, feature_2])
-#
-#         batch_x = torch.from_numpy(np.array(batch_x))
-#
-#         return batch_x
-#
-#     def forward(self, x):
-#         x = self.feature_engineering(x)
-#         return self.model(x.float())
-#
-#     def reset_parameters(self):
-#         self.model.reset_parameters()
-
 
 class VQAStandard(nn.Module):
-    def __init__(self, desc_input_shape, img_input_shape, num_output_classes, use_bias, hidden_size, embedding_matrix):
+    def __init__(self, desc_input_shape, img_input_shape, num_output_classes, use_bias, hidden_size,
+                 encoder_output_size, embedding_matrix):
         super(VQAStandard, self).__init__()
         self.desc_input_shape = desc_input_shape
         self.img_input_shape = img_input_shape
         self.num_classes = num_output_classes
         self.use_bias = use_bias
         self.hidden_size = hidden_size
+        self.out_features = encoder_output_size
         self.layer_dict = nn.ModuleDict()
         self.embedding_layer = self.create_embedding_layer(embedding_matrix)
         self.build_model()
@@ -94,8 +32,8 @@ class VQAStandard(nn.Module):
         self.layer_dict['second_lstm_cell'] = nn.LSTMCell(input_size=self.hidden_size, hidden_size=self.hidden_size,
                                                           bias=self.use_bias)
 
-        self.layer_dict['desc_fc'] = nn.Linear(in_features=4 * self.hidden_size, out_features=1024, bias=self.use_bias)
-        self.layer_dict['img_fc'] = nn.Linear(in_features=self.img_input_shape[1], out_features=1024,
+        self.layer_dict['desc_fc'] = nn.Linear(in_features=4 * self.hidden_size, out_features=self.out_features, bias=self.use_bias)
+        self.layer_dict['img_fc'] = nn.Linear(in_features=self.img_input_shape[1], out_features=self.out_features,
                                               bias=self.use_bias)
 
     def forward(self, input):
@@ -133,28 +71,28 @@ class VQAStandard(nn.Module):
 
 
 class SiameseNetwork(nn.Module):
-    def __init__(self, item_1_model, item_2_model, use_bias=True):
+    def __init__(self, item_1_model, item_2_model, encoder_output_size, fc1_size, fc2_size, use_bias=True):
         super(SiameseNetwork, self).__init__()
         self.item_1_model = item_1_model
         self.item_2_model = item_2_model
         self.bias = use_bias
+        self.in_features = encoder_output_size
+        self.fc1_size = fc1_size
+        self.fc2_size = fc2_size
         self.layer_dict = nn.ModuleDict()
         self.build_model()
 
     def build_model(self):
-        self.layer_dict['fcn1'] = nn.Linear(in_features=1024, out_features=512, bias=self.bias)
-        self.layer_dict['relu1'] = nn.ReLU(inplace=True)
-        self.layer_dict['fcn2'] = nn.Linear(in_features=512, out_features=2056, bias=self.bias)
-        self.layer_dict['relu2'] = nn.ReLU(inplace=True)
-        self.layer_dict['fcn3'] = nn.Linear(in_features=2056, out_features=2, bias=self.bias)
+        self.layer_dict['fcn1'] = nn.Linear(in_features=self.in_features, out_features=self.fc1_size, bias=self.bias)
+        self.layer_dict['fcn2'] = nn.Linear(in_features=self.fc1_size, out_features=self.fc2_size, bias=self.bias)
+        self.layer_dict['fcn3'] = nn.Linear(in_features=self.fc2_size, out_features=1, bias=self.bias)
 
     def forward_once(self, input):
         out = input
         out = self.layer_dict['fcn1'](out)
-        out = self.layer_dict['relu1'](out)
+        out = nn.ReLU(inplace=True)(out)
         out = self.layer_dict['fcn2'](out)
-        out = self.layer_dict['relu2'](out)
-        out = self.layer_dict['fcn3'](out)
+        out = nn.ReLU(inplace=True)(out)
         return out
 
     def forward(self, input):
@@ -171,48 +109,18 @@ class SiameseNetwork(nn.Module):
 
         output_1 = self.forward_once(output_1)
         output_2 = self.forward_once(output_2)
+        # Compute distance between the outputs of the Siamese model
+        distance = torch.abs(output_1 - output_2)
+        # Weight the component-wise distance between the two feature vectors with learnable parameters
+        out = self.layer_dict['fcn3'](distance)
+        # Apply a sigmoid function to the output
+        out = F.sigmoid(out)
 
-        return output_1, output_2
+        return out
 
-    def reset_parameteres(self):
+    def reset_parameters(self):
         self.item_1_model.reset_parameters()
         self.item_2_model.reset_parameters()
         for item in self.layer_dict.children():
             item.reset_parameters()
 
-
-class ContrastiveLoss(nn.Module):
-    def __init__(self, margin=2.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, output_1, output_2, label):
-        # Find the pairwise distance or euclidian distance of two output feature vectors
-        euclidian_distance = F.pairwise_distance(output_1, output_2)
-        # perform constrastive loss calculation with the distsance
-        loss_constrastive = torch.mean((1 - label) * torch.pow(euclidian_distance, 2) + (label) * torch.pow(
-            torch.clamp(self.margin - euclidian_distance, min=0.0), 2))
-
-        return loss_constrastive
-
-# seed = np.random.RandomState(seed=124)
-# embedding_matrix = np.load('dataset/fasttext_embed_10000.npy')
-# model_1 = VQAStandard(desc_input_shape=(64, 102),
-#                     img_input_shape=(64, 2048),
-#                     num_output_classes=2,
-#                     use_bias=True,
-#                     hidden_size=512,
-#                     embedding_matrix=embedding_matrix)
-#
-# model_2 = VQAStandard(desc_input_shape=(64, 102),
-#                     img_input_shape=(64, 2048),
-#                     num_output_classes=2,
-#                     use_bias=True,
-#                     hidden_size=512,
-#                     embedding_matrix=embedding_matrix)
-#
-# desc, img_embed = torch.from_numpy(np.random.randint(100, size=(64, 102))).type(torch.long), \
-#                   torch.from_numpy(np.random.randn(64, 2048)).type(torch.float)
-#
-# siamese_model = SiameseNetwork(model_1, model_2, use_bias = True)
-# siamese_model([desc, img_embed, desc, img_embed])
