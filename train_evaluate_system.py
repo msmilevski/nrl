5026ca9f179201fcd8655e52268b84c6c5b4f89c
msmilevski <m.smilevski57@gmail.com>
2019-07-19 15:26:37 +0100

    Add code for neural models, change directories for files

--- train_evaluate_system.py
+++ train_evaluate_system.py
@@ -2,7 +2,7 @@ from data_provider import DatasetProvider
 import numpy as np
 from arg_extractor import get_args
 from experiment_builder import ExperimentBuilder
-from models import BaselineModel
+from models import SiameseNetwork, VQAStandard
 
 args, device = get_args()  # get arguments from command line
 rng = np.random.RandomState(seed=args.seed)  # set the seeds for the experiment
@@ -13,52 +13,52 @@ from torch.utils.data import DataLoader
 
 torch.manual_seed(seed=args.seed)  # sets pytorch's seed
 
-print("Loading dataset ...")
+print("Loading datasets ...")
 print(args.dataset_name)
-if args.dataset_name == 'baseline':
+# Initialize random seed
+seed = np.random.RandomState(seed=args.seed)
 
+if args.dataset_name == 'standard':
     training_data = DatasetProvider(pair_file_path='dataset/ItemPairs_train_processed.csv',
-                                    data_file_path='dataset/fasttext_data.hdf5',
-                                    images_dir='dataset/resnet152/')
+                                    data_file_path='/disk/scratch/s1885778/dataset/fasttext_data.hdf5',
+                                    images_dir='/disk/scratch/s1885778/dataset/resnet152_1')
     training_data = DataLoader(training_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
     print('Training set loaded.')
     valid_data = DatasetProvider(pair_file_path='dataset/ItemPairs_val_processed.csv',
-                                    data_file_path='dataset/fasttext_data.hdf5',
-                                    images_dir='dataset/resnet152/')
+                                 data_file_path='/disk/scratch/s1885778/dataset/fasttext_data.hdf5',
+                                 images_dir='/disk/scratch/s1885778/dataset/resnet152_1')
     valid_data = DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
     print('Validation set loaded.')
     test_data = DatasetProvider(pair_file_path='dataset/ItemPairs_test_processed.csv',
-                                    data_file_path='dataset/fasttext_data.hdf5',
-                                    images_dir='dataset/resnet152/')
-    test_data = DataLoader(test_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
-    print('Test set loaded.')
-
-elif args.dataset_name == 'standard':
-    training_data = DatasetProvider(pair_file_path='dataset/ItemPairs_train_processed.csv',
-                                    data_file_path='dataset/fasttext_data.hdf5',
-                                    images_dir='dataset/resnet152/')
-    training_data = DataLoader(training_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
-    print('Training set loaded.')
-    valid_data = DatasetProvider(pair_file_path='dataset/ItemPairs_val_processed.csv',
-                                 data_file_path='dataset/fasttext_data.hdf5',
-                                 images_dir='dataset/resnet152/')
-    valid_data = DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
-    print('Validation set loaded.')
-    test_data = DatasetProvider(pair_file_path='dataset/ItemPairs_test_processed.csv',
-                                data_file_path='dataset/fasttext_data.hdf5',
-                                images_dir='dataset/resnet152/')
+                                data_file_path='/disk/scratch/s1885778/dataset/fasttext_data.hdf5',
+                                images_dir='/disk/scratch/s1885778/dataset/resnet152_1')
     test_data = DataLoader(test_data, batch_size=args.batch_size, shuffle=True, num_workers=0)
     print('Test set loaded.')
 
 # Binary classification
 num_output_classes = 2
+embedding_matrix = np.load('dataset/fasttext_embed_10000.npy')
+if args.model_name == 'standard':
+    model_1 = VQAStandard(desc_input_shape=(args.batch_size, 102),
+                        img_input_shape=(args.batch_size, 2048),
+                        num_output_classes=num_output_classes,
+                        use_bias=True,
+                        hidden_size=args.lstm_hidden_dim,
+                        encoder_output_size=args.encoder_output_size,
+                        embedding_matrix=embedding_matrix)
 
-if args.model_name == 'baseline':
-    model = BaselineModel(input_dim=2)
-    print('Baseline model built.')
+    model_2 = VQAStandard(desc_input_shape=(args.batch_size, 102),
+                        img_input_shape=(args.batch_size, 2048),
+                        num_output_classes=num_output_classes,
+                        use_bias=True,
+                        hidden_size=args.lstm_hidden_dim,
+                        encoder_output_size=args.encoder_output_size,
+                        embedding_matrix=embedding_matrix)
 
 
-experiment = ExperimentBuilder(network_model=model,
+siamese_model = SiameseNetwork(model_1, model_2, encoder_output_size=args.encoder_output_size, use_bias = True)
+
+experiment = ExperimentBuilder(network_model=siamese_model,
                                     experiment_name=args.experiment_name,
                                     num_epochs=args.num_epochs,
                                     weight_decay_coefficient=args.weight_decay_coefficient,
@@ -66,6 +66,5 @@ experiment = ExperimentBuilder(network_model=model,
                                     device=device,
                                     train_data=training_data,
                                     val_data=valid_data,
-                                    test_data=test_data,
-                                    isBaseline=True)  # build an experiment object
+                                    test_data=test_data)  # build an experiment object
 experiment_metrics, test_metrics = experiment.run_experiment()  # run experiment and return experiment metrics
